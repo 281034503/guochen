9fa5fa6301f752648905ad2a40c71b54128aea0a
281034503 <281034503@qq.com>
2017-04-15 22:56:18 +0800

    Change-Id: I30b09fcfcce681fbdd21e5eaa0bbf858fcd50bbb

---
+++ src/demo/ch01/Helloworld.java
@@ -0,0 +1,8 @@
+package demo.ch01;
+
+public class Helloworld {
+	public static void main(String[]args){
+	System.out.println("Helloworld");
+	
+	}
+}
---
+++ src/demo/ch02/TMsl201.java
@@ -0,0 +1,8 @@
+package demo.ch02;
+
+public class TMsl201 {
+	public static void main(String[] args){
+	System.out.println("《Java从入门到精通》");
+	System.out.println("是我学习Java语言的最佳书籍");
+	}
+}
