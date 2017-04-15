d28a682f25625fa722a329cc9dc0ce5c6be3a9a3
281034503 <281034503@qq.com>
2017-04-15 23:36:44 +0800

    Change-Id: I404938a347a0fd758e9591fd3ebcab307b6d0891

---
+++ src/demo/ch03/Tmsl301.java
@@ -0,0 +1,12 @@
+package demo.ch03;
+
+public class Tmsl301 {
+
+	static String s1 = "ÄãºÃ";
+    public static void main(String[] arge){
+		String s2 = "Java";
+				System.out.println(s1);
+		System.out.println(s2);
+	}
+
+}
---
+++ src/demo/ch03/Tmsl303.java
@@ -0,0 +1,16 @@
+package demo.ch03;
+
+public class Tmsl303 {    //定义类
+
+	public static void main(String[] args) {   //主方法
+		char word ='d', word2 ='@';   //定义char型变量
+		int p =23045, p2 = 45213;  //定义 int 型变量
+System.out.println("d在 unicode 表中的顺序位置是: " + (int) word);
+System.out.println("@在unicode 表中的顺序位置是: " + (int) word2);
+System.out.println("unocode 表中的第 23045 位是: " + (char) p);
+System.out.println("unicode 表中的第45213 位是: " + (char) p2);
+
+}
+
+
+}
