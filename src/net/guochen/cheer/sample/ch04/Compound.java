b59bfcb5627a944c198f9743e8c1463df556ba98
281034503 <281034503@qq.com>
2017-04-17 20:21:50 +0800

    Change-Id: I7d0b4cf346de16070b7fc4724f97ead51f1d0aa9

---
+++ src/net/guochen/cheer/sample/ch04/Compound.java
@@ -0,0 +1,21 @@
+package net.guochen.cheer.sample.ch04;
+
+public class Compound {
+	public static void main(String[] arg){
+		int x=20;
+		{
+			int y=40;
+			System.out.println(y);
+			int z=245;
+			boolean b;
+			{
+				b=y>z;
+				System.out.println(b);
+				
+			}
+		}
+		String word="hello jav";
+		System.out.println(word);
+	}
+
+}
