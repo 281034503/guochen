58f9d9ebf7e7014328e3f9b311721c2d9af53cd0
281034503 <281034503@qq.com>
2017-04-19 14:43:57 +0800

    Change-Id: I346ae306d69f737bac097b192dd5ad4f75851039

---
+++ src/net/guochen/cheer/sample/ch04/Circulate.java
@@ -0,0 +1,17 @@
+package net.guochen.cheer.sample.ch04;
+
+public class Circulate {   //创建类Circulate
+
+	public static void main(String[] args) {   //主方法
+		int sum=0;   //声明变量，用于保存各数相加后的结果
+		for(int i=2;i<=100;i+=2){  
+			
+	       sum=sum+i;    //指定循环条件及循环体
+		}
+		//将相加后的结构输出
+		System.out.println("2到100之间的所有的偶数之和为"+sum);
+	}
+		}
+
+
+
