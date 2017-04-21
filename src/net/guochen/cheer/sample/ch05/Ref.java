e547f6633a9f8e14820e57e89a64c9b7f8c3bc58
281034503 <281034503@qq.com>
2017-04-21 17:31:38 +0800

    Change-Id: I5ef5e3ac3beec02da0e61cf77ac7d831ada25273

---
+++ src/net/guochen/cheer/sample/ch05/Ref.java
@@ -0,0 +1,14 @@
+package net.guochen.cheer.sample.ch05;
+
+public class Ref {   //创建类
+
+	public static void main(String[] args) {    //主方法
+		String str="hello word";    //定义字符串str
+		char mychar=str.charAt(8);  
+		//将字符串str中的索引位置是6的字符串返回
+		System.out.println("字符串str中索引位置是6的字符串为："+mychar);
+		//输出信息
+
+	}
+
+}
