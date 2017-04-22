f4ba14afaf8b84e3b81921941eb78f642696f612
281034503 <281034503@qq.com>
2017-04-22 20:00:47 +0800

    Change-Id: Iaf305609327a6ae1b60d0c69270386ee236b3bfe

---
+++ src/net/guochen/cheer/excrcise/ch04/One.java
@@ -0,0 +1,16 @@
+package net.guochen.cheer.excrcise.ch04;
+
+public class One {
+	public static void main(String[] args){
+		int x=51;
+		if(x%2==0){
+			System.out.println("x是偶数");
+	}
+		else{
+			System.out.println("x是奇数");
+		}
+			
+	}
+	
+
+}
