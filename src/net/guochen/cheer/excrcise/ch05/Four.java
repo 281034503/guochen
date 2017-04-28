2d97eb0b7e66beb7df48a29a2009b93482801f35
281034503 <281034503@qq.com>
2017-04-28 16:43:29 +0800

    Change-Id: I63b26b3035f4aeb6a35b804f1d71bbc0dae8fda9

--- src/net/guochen/cheer/excrcise/ch05/Four.java
+++ src/net/guochen/cheer/excrcise/ch05/Four.java
@@ -4,11 +4,11 @@ public class Four {
 
 	public static void main(String[] args) {
 		
-		StringBuilder a1=new StringBuilder("");
+		StringBuilder a1=new StringBuilder("");    //创建字符串生成器
 		
-		for(int i=1;i<=10;i++) {
+		for(int i=1;i<=10;i++) {     //for循环操作
 			
-			a1.append(i);
+			a1.append(i);    //循环追加字符
 			 
 		}
 		System.out.println(a1.toString());
