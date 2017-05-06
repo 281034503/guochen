ec4d90a22df0d29736c4d7218b0f2a13b6e2dde3
281034503 <281034503@qq.com>
2017-05-06 15:00:30 +0800

    Change-Id: I73e0af88269374e3a52f3696eed3ba0d0d4ac1b4

--- src/net/guochen/cheer/sample/ch09/DecimalFormatSimpleDemo.java
+++ src/net/guochen/cheer/sample/ch09/DecimalFormatSimpleDemo.java
@@ -1,9 +1,14 @@
 package net.guochen.cheer.sample.ch09;
 
+import java.text.DecimalFormat;
+
 public class DecimalFormatSimpleDemo {
 	//使用实例化对象时设置格式化模式
 	static public void SimgleFormat(String pattern,double value){
 		//实例化DecimalFormat对象
+		DecimalFormat myFormat=new DecimalFormat(pattern);
+		String output=myFormat.format(value);
+		System.out.println(value+""+pattern+""+output);
 	}
 
 }
