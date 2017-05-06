b8f12037d6f15621709cb84fe82d12affa8ce4e1
281034503 <281034503@qq.com>
2017-05-06 14:49:41 +0800

    Change-Id: I5306e9a673b748af2130ef3698fb8bb108ad0cf8

---
+++ src/net/guochen/cheer/sample/ch09/DecimalFormatSimpleDemo.java
@@ -0,0 +1,9 @@
+package net.guochen.cheer.sample.ch09;
+
+public class DecimalFormatSimpleDemo {
+	//使用实例化对象时设置格式化模式
+	static public void SimgleFormat(String pattern,double value){
+		//实例化DecimalFormat对象
+	}
+
+}
