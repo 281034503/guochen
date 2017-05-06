70344f137ffb51ac9fd5c1ac685653308e256021
281034503 <281034503@qq.com>
2017-05-06 15:41:08 +0800

    Change-Id: I8553990a1c37a4fbff7ef44590865da43a825820

--- src/net/guochen/cheer/sample/ch09/DecimalFormatSimpleDemo.java
+++ src/net/guochen/cheer/sample/ch09/DecimalFormatSimpleDemo.java
@@ -7,8 +7,19 @@ public class DecimalFormatSimpleDemo {
 	static public void SimgleFormat(String pattern,double value){
 		//实例化DecimalFormat对象
 		DecimalFormat myFormat=new DecimalFormat(pattern);
-		String output=myFormat.format(value);
+		String output=myFormat.format(value);    //将数字进行格式化
 		System.out.println(value+""+pattern+""+output);
+		
+	}
+	
+	//使用applyPattern()方法对数字进行格式化
+	static public void UseApplyPatternMethodFormat(String pattern,double value){
+		DecimalFormat myFormat=new DecimalFormat();
+		myFormat.applyPattern(pattern);
+		System.out
+		           .println();
+		
+		
 	}
 
 }
