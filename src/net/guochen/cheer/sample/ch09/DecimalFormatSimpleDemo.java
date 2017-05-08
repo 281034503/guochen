66d51816462990ae72c8e9946496f859b38bacf2
281034503 <281034503@qq.com>
2017-05-08 15:09:39 +0800

    Change-Id: Ib72fa10141f1d43edb0db39568d65cad0e16a937

--- src/net/guochen/cheer/sample/ch09/DecimalFormatSimpleDemo.java
+++ src/net/guochen/cheer/sample/ch09/DecimalFormatSimpleDemo.java
@@ -17,7 +17,19 @@ public class DecimalFormatSimpleDemo {
 		DecimalFormat myFormat=new DecimalFormat();
 		myFormat.applyPattern(pattern);
 		System.out
-		           .println();
+		           .println(value+""+pattern+""+myFormat.format(value));
+	}
+	public static void main(String[] ages){
+		SimgleFormat("###,###,###",123456.789);    //调用静态SimgleFormat()方法
+		SimgleFormat("00000000.###kg",123456.789);   //在数字后加上单位
+		//按照格式模板格式化数字，不存在的位以0显示
+		SimgleFormat("000000.000",123.78);
+		//调用静态UseApplypatternMethodFormat()方法
+		UseApplyPatternMethodFormat("#.###%",0.789);
+		//将小数点后格式化位两位
+		UseApplyPatternMethodFormat("###.##",123456.789);
+		//将数字转化位千分数形式
+		UseApplyPatternMethodFormat("0.00\u2030",0.789);
 		
 		
 	}
