30ab91c6ecc61b1725bd6aa107c6d65593691d11
281034503 <281034503@qq.com>
2017-12-12 22:23:10 +0800

    Change-Id: I9d86acbc362ebb21c3edc2b4f0e2b5aa43dab2ac

--- src/net/guochen/cheer/sample/ch06/Swap.java
+++ src/net/guochen/cheer/sample/ch06/Swap.java
@@ -4,7 +4,7 @@ import java.util.Arrays;   //导入java.util.Arrays类方法
 
 public class Swap {        //创建类
 	public static void main(String[] args){     //主方法
-		int arr[]=new int[5];    //创建int型一维数组，长度为5
+		int arr[]=new int[7];    //创建int型一维数组，长度为5
 		Arrays.fill(arr, 8);    //使用同一值对数组进行填满，
 		//Arrays.fill 数组填充
 		for(int i=0;i<arr.length;i++){    //循环遍历数组中的元素
